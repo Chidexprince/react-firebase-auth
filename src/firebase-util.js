d78c3989a2fb40b4c0bc9f514888b44ddfda7e47
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2022-02-07 04:59:02 +0100

    initial commit of base files

---
+++ src/firebase-util.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+​​import {
+​​  GoogleAuthProvider,
+​​  getAuth,
+​​  signInWithPopup,
+​​  signInWithEmailAndPassword,
+​​  createUserWithEmailAndPassword,
+​​  signOut,
+​​} from "firebase/auth";
+​​import {
+​​  getFirestore,
+​​  query,
+​​  getDocs,
+​​  collection,
+​​  where,
+​​  addDoc,
+​​} from "firebase/firestore";
